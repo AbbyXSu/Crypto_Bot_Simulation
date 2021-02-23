aafcdfcf90673d2f1faa333bb17ac8d250e7590b
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-23 11:02:00 +0000

    feat/3 fully functional frontend and backend with prepresisited MYSQL database and docker-compose integrated via docker

--- application/report_UI/app.py
+++ application/report_UI/app.py
@@ -1,12 +1,21 @@
-from Flask import app
+from flask import Flask
 import requests
 from flask import render_template
 
+app = Flask(__name__)
+
 @app.route('/',methods=["GET"])
 def index():  
-    reports, latestItem = requests.get("http://localhost:5004/reports")
+    result_raw = requests.get("http://report_engine:5004/report_engine")
+    if result_raw.status_code == 200 or result_raw.status_code == 201:
+        result = result_raw.json()
+    else:
+        result = {}
 
-    return render_template("index.html",reports = reports.text, latestItem = latestItem.text)
+    reports = result.get('reports', '')
+    latestItem = result.get('latest_item', '')
+
+    return render_template("index.html",reports = reports, latestItem = latestItem)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
