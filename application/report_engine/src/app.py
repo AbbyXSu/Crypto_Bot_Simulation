de5e81addc4f685cd249eb33388ed97d5342ca2c
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-23 11:23:47 +0000

    feat3 code clean up and improve

--- application/report_engine/src/app.py
+++ application/report_engine/src/app.py
@@ -1,13 +1,9 @@
 from sqlalchemy import desc
 import requests
 from flask import json, jsonify
-from flask import Response
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
-from sqlalchemy.ext.declarative import DeclarativeMeta
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '88dd6a6854b7f1901b7f01d353186c6a'
@@ -27,28 +23,6 @@ class Reports(db.Model):
     efficiency_ratio = db.Column(db.String(500), nullable=False)
 
 db.create_all()
-
-
-
-
-# class AlchemyEncoder(json.JSONEncoder):
-
-#     def default(self, obj):
-#         if isinstance(obj.__class__, DeclarativeMeta):
-#             # an SQLAlchemy class
-#             fields = {}
-#             for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-#                 data = obj.__getattribute__(field)
-#                 try:
-#                     json.dumps(data) # this will fail on non-encodable values, like other classes
-#                     fields[field] = data
-#                 except TypeError:
-#                     fields[field] = None
-#             # a json-encodable dict
-#             return fields
-#         print('***********')
-#         return json.JSONEncoder.default(self, obj)
-
 
 def row_dict(row):
     d = {}
