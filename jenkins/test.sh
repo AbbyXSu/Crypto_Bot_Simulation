5cd5f968d2a997e9b835d3f96e75a913e6aa2774
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-25 17:55:47 +0000

    unit test improvement

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,25 +1,25 @@
-cd application/expenditure_service
+cd application/ expenditure_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml 
-cd ..
+cd .. /..
 
-cd report_aggregate_service
+cd report_aggregate_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml 
-cd ..
+cd ../..
 
 
-cd report_UI
+cd report_UI/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml 
-cd ..
+cd ../..
 
 
-cd revenue_service
+cd revenue_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml 
-cd ..
+cd ../../..
