51ac82a9fedf91b096721b4d9177a4ec7335a094
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-25 18:18:32 +0000

    unit test fix

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,25 +1,25 @@
-cd application/expenditure_service/tests
+cd expenditure_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=expenditure_service --cov-report 
-cd .. /..
+cd .. 
 
-cd report_aggregate_service/tests
+cd report_aggregate_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=report_aggregate_service --cov-report 
-cd ../..
+cd ..
 
 
-cd report_UI/tests
+cd report_UI
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=report_UI --cov-report 
-cd ../..
+cd ..
 
 
-cd revenue_service/tests
+cd revenue_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=revenue_service --cov-report  
-cd ../../..
+cd ..
