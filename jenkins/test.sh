6279ae9731848d3b43460807a34d0684581edc33
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-25 18:11:10 +0000

    unittest improv

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,25 +1,25 @@
 cd application/expenditure_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=expenditure_service --cov-report xml 
+python3 -m pytest --cov=expenditure_service --cov-report 
 cd .. /..
 
 cd report_aggregate_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=report_aggregate_service --cov-report xml 
+python3 -m pytest --cov=report_aggregate_service --cov-report 
 cd ../..
 
 
 cd report_UI/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=report_UI --cov-report xml 
+python3 -m pytest --cov=report_UI --cov-report 
 cd ../..
 
 
 cd revenue_service/tests
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=revenue_service --cov-report xml 
+python3 -m pytest --cov=revenue_service --cov-report  
 cd ../../..
