7114a3c2d45c0a6bd63ee8ed39155db5a2f3fbe1
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-25 18:30:27 +0000

    unitest fix

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,26 +1,26 @@
 cd application/expenditure_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=expenditure_service 
+python3 -m pytest --cov=expenditure_service --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ../.. 
 
 cd application/report_aggregate_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=report_aggregate_service 
+python3 -m pytest --cov=report_aggregate_service --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ../..
 
 
 cd application/report_UI
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=report_UI  
+python3 -m pytest --cov=report_UI  --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ../..
 
 
 cd application/revenue_service
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov requests_mock
-python3 -m pytest --cov=revenue_service   
+python3 -m pytest --cov=revenue_service   --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ../..
 
