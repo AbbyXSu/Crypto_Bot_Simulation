2498e424e13e78483779351823ab82bb1a079d7c
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-27 00:58:59 +0000

    wip

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-curl https://get.docker.com | sudo bash
-sudo apt install -y curl jq
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# curl https://get.docker.com | sudo bash
+# sudo apt install -y curl jq
+# version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker jenkins
-newgrp docker
-docker-compose build --parallel
-docker-compose push
+sudo chmod 666 /var/run/docker.sock
+
+docker-compose down --rmi all
+docker-compose build
+sudo docker login -u ${USERNAME} -p ${PASSWORD} https://registry.hub.docker.com
+sudo docker-compose push
+
