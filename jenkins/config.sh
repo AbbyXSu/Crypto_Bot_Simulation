5841522578f3ddc18225e177805019e3c2d4304a
AbbyXSu <xiaotong.s.abby@gmail.com>
2021-02-28 14:38:20 +0000

    WIP jenkins conf

--- jenkins/config.sh
+++ jenkins/config.sh
@@ -5,10 +5,5 @@
 # sudo su - jenkins, install ansible on this machine for jenkins
 # jenkins runs playbook
 # make sure ~/.local/bin exists and is on your PATH
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-# install ansible with pip
-pip3 install --user ansible
 cd ansible
 ansible-playbook -i inventory.ini playbook.yaml
